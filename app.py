0a1713d185f40238ed53bcf18916b5534261c07b
sriram <159551306+sriram-65@users.noreply.github.com>
2025-03-05 17:52:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask , render_template , request , jsonify
 from flask_cors import CORS
 import google.generativeai as genai
-
+from pymongo import MongoClient
+import datetime
 
 app = Flask(__name__)
 CORS(app)
 genai.configure(api_key="AIzaSyAvyLEzkIaibw5BFF4ZCISLljZNbLKd2Cg")
 model = genai.GenerativeModel("gemini-2.0-flash")
+
+client = MongoClient("mongodb+srv://sriram65raja:1324sriram@cluster0.dejys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client["ai"] 
+chat_collection = db["UserChats"]  
 
 @app.route("/")
 def home():
@@ -24,6 +29,12 @@ def Docs():
 def chat():
     user_meassage = request.form["meassage"]
     ai_res = model.generate_content(user_meassage)
+    chat_data = {
+        "UserMeassage":user_meassage,
+        "AI_Response": ai_res.text,
+        "Time":datetime.datetime.today()
+    }
+    chat_collection.insert_one(chat_data)
     return render_template("index.html" , chat=str(ai_res.text))
 
 
@@ -35,7 +46,8 @@ def api(prompt):
           res = model.generate_content(prompt)
           return jsonify(res.text)
         
-  
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
